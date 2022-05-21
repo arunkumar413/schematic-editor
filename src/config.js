36797e74e94b571e7b50e50b9020a71caca39c8e
Arun Kumar <arunkumar413@gmail.com>
2022-05-21 13:37:30 +0000

    adding config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+// This file holds the common configuration for the application.
+
+export const schematicConfig = {
+    pinLength: 30,// pin path length
+    pinCircleRadius: 3, // pin circle radius
+    IcRectWidth: 100,
+    pinVerticalGap: 10
+
+}
