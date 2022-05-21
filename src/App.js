2f79b450e6cd549f3612eaf6613d9172a415c41d
Arun Kumar <arunkumar413@gmail.com>
2022-05-21 13:33:04 +0000

    able to add the connections list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 import Snap from 'snapsvg-cjs';
 import { EightPin } from '../src/components/eightPin'
 import { Resistor } from './components/Resistor';
@@ -8,7 +15,14 @@ import './style.css';
 
 export default function App() {
 
+  const [connnections, setConnections] = useState([]);
+  const [clickcount, setClickcount] = useState(0)
   let ref = useRef(null)
+
+
+  var count = 0;
+  var from = '';
+  var to = '';
 
 
   useEffect(function () {
@@ -18,21 +32,50 @@ export default function App() {
       item.drag();
     })
 
+    s.selectAll('.node').forEach(function (item, index) {
+
+
+      item.click(function (e) {
+        ++count
+
+        if (count === 1) {
+          debugger
+
+          from = item.attr('id')
+
+        }
+
+        else if (count === 2) {
+          debugger
+          to = item.attr('id')
+          setConnections(function (prevState) {
+            return [...prevState, { from: from, to: to }]
+          })
+          count = 0
+
+        }
+
+
+
+
+      })
+
+    })
 
 
   })
 
-
-
-
   return (
-    <svg ref={ref} id="svg-container" width={1000} height={1000}>
-      {/* {rectElements} */}
+    <RecoilRoot>
 
-      <EightPin name='8 pin IC' />
-      <SixteenPin name='16 pin IC' />
-      <Resistor name='R1'/>
+      <svg ref={ref} id="svg-container" width={1000} height={1000}>
+        {/* {rectElements} */}
 
-    </svg>
+        <EightPin name='8 pin IC' />
+        <SixteenPin name='16 pin IC' />
+        <Resistor name='R1' />
+
+      </svg>
+    </RecoilRoot>
   );
 }
